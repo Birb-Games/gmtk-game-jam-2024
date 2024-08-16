4da0e8d3ca139ada5db21477045d4edacd6bc31e
Colin <cfinn68916@gmail.com>
2024-08-16 14:48:33 -0500

    Add coin HUD
    Signed-off-by: Colin <cfinn68916@gmail.com>

---
+++ scripts/HUD.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+
+var coins=1000
+
+func publishCoins():
+	$CoinLabel.text="Coins: "+str(coins)
+
+func getCoins():
+	return coins
+func addCoins(coinAmt):
+	coins+=coinAmt
+	publishCoins()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	publishCoins()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
