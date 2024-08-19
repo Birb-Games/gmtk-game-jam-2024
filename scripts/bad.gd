4da4d9ade2fa60da4b5ed5f650f7d21adf684deb
JL <jeremyli.gitemail@gmail.com>
2024-08-19 13:15:10 -0500

    changes to HUD

--- scripts/bad.gd
+++ scripts/bad.gd
@@ -1,31 +1,18 @@
 extends Area2D
 
-func reset():
-	$MoveableItem.stop = false
-	$MoveableItem.direction = Vector2i.ZERO
-	$MoveableItem.new_tile = true
-	set_process(true)
-	show()
-	
-func remove():
-	hide()
-	set_process(false)
-	get_tree().get_root().get_node("Root/GameScreen/Spawner").bad_pool.append(self)
-	get_tree().get_root().get_node("Root/GameScreen/Requests").remove_child(self)
-
 func _on_moveable_item_deleter() -> void:
 	$/root/Root/GameScreen.spawn_counts["bad"] -= 1
-	remove()
+	queue_free()
 
 func _on_moveable_item_output() -> void:
 	$/root/Root/GameScreen.spawn_counts["bad"] -= 1
 	$/root/Root/GameScreen.add_coins(-320)
-	remove()
+	queue_free()
 
 func _on_moveable_item_server() -> void:
 	$/root/Root/GameScreen.spawn_counts["bad"] -= 1
 	$/root/Root/GameScreen.add_coins(-80)
-	remove()
+	queue_free()
 
 func _on_moveable_item_empty() -> void:
 	var i = randi() % len($/root/Root/GameScreen.input_pipes)
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -2,6 +2,10 @@ extends CanvasLayer
 
 var selected: String=""
 
+var upgrade_get_cost = 200
+var upgrade_bad_cost = 800
+var upgrade_download_cost = 200
+
 func defocus():
 	selected=""
 	$Inventory/InButton.release_focus()
@@ -42,7 +46,7 @@ func _process(delta):
 	if(Input.is_action_just_pressed("defocus")):
 		defocus()
 	
-	if(Input.is_action_just_pressed("quit")):
+	if(Input.is_action_just_pressed("quit") and !$GameOver.visible):
 		$Quit.visible = !$Quit.visible
 		if $Quit.visible:
 			get_tree().paused = true
@@ -92,15 +96,27 @@ func _on_bridge_button_pressed() -> void:
 	select("bridge")
 
 func set_count_text(label: Label, id: String):
+	var percent_to_max = float($/root/Root/GameScreen.spawn_counts[id]) / float($/root/Root/GameScreen.max_counts[id])
+	if percent_to_max > 0.8:
+		label.modulate = Color.RED
+	else:
+		label.modulate = Color.WHITE
 	label.text = str($/root/Root/GameScreen.spawn_counts[id])
-	label.text += "/" + str($/root/Root/GameScreen.max_counts[id])
+	label.text += "/MAX " + str($/root/Root/GameScreen.max_counts[id])
 	label.text += " (" + str(int(round($/root/Root/GameScreen/Spawner.timers[id]))) + "s)"
+
+func set_upgrade_text(button: Button, cost: int):
+	button.text = "+MAX: $" + str(cost)
 	
 func update_text():
-	set_count_text($Counts/GetCount, "get")
-	set_count_text($Counts/BadCount, "bad")
-	set_count_text($Counts/DownloadCount, "download")
+	set_count_text($Counts/Get/GetCount, "get")
+	set_count_text($Counts/Bad/BadCount, "bad")
+	set_count_text($Counts/Download/DownloadCount, "download")
 	$Counts/RetCount.text = str($/root/Root/GameScreen.spawn_counts["return"])
+	
+	set_upgrade_text($Counts/Get/UpgradeGet, upgrade_get_cost)
+	set_upgrade_text($Counts/Bad/UpgradeBad, upgrade_bad_cost)
+	set_upgrade_text($Counts/Download/UpgradeDownload, upgrade_download_cost)
 
 	if $/root/Root/GameScreen.tile_costs.has(selected):
 		$Cost.text = "Cost: $" + str($/root/Root/GameScreen.tile_costs[selected])
@@ -119,3 +135,25 @@ func _on_return_to_menu_pressed() -> void:
 func _on_no_pressed() -> void:
 	$Quit.hide()
 	get_tree().paused = false
+
+func _on_view_factory_pressed() -> void:
+	$GameOver.hide()
+	$Inventory.hide()
+	$CoinLabel.hide()
+	$Counts.hide()
+	$Cost.hide()
+
+func _on_upgrade_get_pressed() -> void:
+	if $/root/Root/GameScreen.spend_coins(upgrade_get_cost):
+		upgrade_get_cost *= 4
+		$/root/Root/GameScreen.max_counts["get"] *= 2
+
+func _on_upgrade_bad_pressed() -> void:
+	if $/root/Root/GameScreen.spend_coins(upgrade_bad_cost):
+		upgrade_bad_cost *= 4
+		$/root/Root/GameScreen.max_counts["bad"] *= 2
+
+func _on_upgrade_download_pressed() -> void:
+	if $/root/Root/GameScreen.spend_coins(upgrade_download_cost):
+		upgrade_download_cost *= 4
+		$/root/Root/GameScreen.max_counts["download"] *= 2
