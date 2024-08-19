4b3cf9cd670b4b44960bb27e5a74c35029ad459a
Colin <cfinn68916@gmail.com>
2024-08-19 15:44:33 -0500

    Fix issue
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- scripts/get.gd
+++ scripts/get.gd
@@ -52,7 +52,7 @@ func _on_area_exited(area: Area2D) -> void:
 	if area.is_in_group("return"):
 		collision_counts -= 1
 
-static var pipe_index;
+static var pipe_index=0;
 
 func _on_moveable_item_empty() -> void:
 	var i = pipe_index % len($/root/Root/GameScreen.input_pipes)
